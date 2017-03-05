d4edc54d276d5394d9c73d81ce95d64eeb15e7c7
Edward Cheadle <edward.cheadle@cambiahealth.com>
2017-03-04 18:08:22 -0700

    Finished adding conf file for nginx repo

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-variable=$(cat <<EOF
+#Need to run with SUDO
+set -x
+OS=centos
+OSRELEASE=7
+REPO=$(cat <<EOF
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/${OS}/${OSRELEASE}/\$basearch/
 gpgcheck=0
 enabled=1
-EOF 
+EOF
 )
-echo "$variable"
-
+#echo "$REPO"
+echo "$REPO" >> /etc/yum.repos.d/nginx.repo
