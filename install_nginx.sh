12084466ef88a71027c91c76f1318b81fa2997a1
Edward Cheadle <edward.cheadle@cambiahealth.com>
2017-03-05 12:48:58 -0700

    Added additional information to install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -13,6 +13,17 @@ gpgcheck=0
 enabled=1
 EOF
 )
+WEBCONF=$(cat <<EOF
+server {
+  server_name localhost;
+
+  location / {
+    root /var/www/hos/html/;
+    index index.html;
+  }
+}
+EOF
+)
 #echo "$REPO"
 echo "$REPO" >> /etc/yum.repos.d/nginx.repo
 rpm --import nginx_signing.key
@@ -27,3 +38,11 @@ echo "--------------------------------------------------------------------"
 ls /var/log/nginx
 ls ls /usr/share/nginx
 ls /etc/nginx
+mkdir /var/www/html
+touch /var/www/html/index.html
+chown -R nginx.nginx /var/www/*
+ps -ef --forest | grep nginx
+#worker_process should equal number of cores
+lscpu
+#Check Conf file
+nginx -t
