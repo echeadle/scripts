77c5ca03ad686239ae98df899d8ca8de582fd982
Edward Cheadle <edward.cheadle@cambiahealth.com>
2017-03-05 12:53:23 -0700

    Added pgit 'git push -u origin master' to aliases

--- mybashrcaliases.sh
+++ mybashrcaliases.sh
@@ -6,4 +6,5 @@ alias rsd="rsync -av --delete"
 #USe ssh to copy Directory to removte server
 # rsync -av –delete -e ssh /Directory1/ geek@192.168.235.137:/Directory2/
 alias rsrm=" rsync -av –delete -e ssh "
-alias ll="ls -la"
+alias ll="ls -la"\
+pgit="git push -u origin master"
